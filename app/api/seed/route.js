1177105767789d95d5be4221d94f4a5544ab8000
Ajay-Shanmugasundram <ajayajpi@gmail.com>
2025-03-09 17:16:38 +0530

    v2

--- app/api/seed/route.js
+++ app/api/seed/route.js
@@ -1,6 +1,6 @@
-import { seedTransactions } from "@/actions/seed"
+// import { seedTransactions } from "@/actions/seed"
 
-export async function GET() {
-    const response = await seedTransactions()
-    return Response.json(response)
-}
+// export async function GET() {
+//     const response = await seedTransactions()
+//     return Response.json(response)
+// }
